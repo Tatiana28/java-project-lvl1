4c23bb72313e22d99849b76f2024ff1f54669f15
Tatiana Nikitina <tatiananikitina@gmail.com>
2021-06-03 12:14:50 +0200

    add changes from step3

---
+++ src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void userMethod() {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        Scanner sc = new Scanner(System.in);
+        String username = sc.nextLine();
+        System.out.println("Hello, " + username + "!");
+    }
+}
