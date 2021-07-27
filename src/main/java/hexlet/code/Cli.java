caeedab992bf4d4ee97cb827be3f42f75a31f707
Tatiana Nikitina <tatiananikitina@gmail.com>
2021-07-27 22:23:18 +0200

    rename method userName for welcomeUser

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Cli {
 
-    public static String userMethod() {
+    public static String welcomeUser() {
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         Scanner sc = new Scanner(System.in);
