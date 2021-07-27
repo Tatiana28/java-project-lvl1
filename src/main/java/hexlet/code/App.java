d58a3351f8fc489b2a3d264aa3fedbb3a1cba7b1
Tatiana Nikitina <tatiananikitina@gmail.com>
2021-07-27 22:13:55 +0200

    use method startGame

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,58 +1,14 @@
 package hexlet.code;
 
-import hexlet.code.games.CalcGame;
-import hexlet.code.games.EvenGame;
-import hexlet.code.games.GcdGame;
-import hexlet.code.games.PrimeGame;
-import hexlet.code.games.ProgressionGame;
-
 import java.util.Scanner;
 
-import static hexlet.code.Cli.getNumberFromInput;
-
 public class App {
-
-    private static final int EXIT_GAME_MENU = 0;
-    private static final int GREET_GAME_MENU = 1;
-    private static final int EVEN_GAME_MENU = 2;
-    private static final int CALC_GAME_MENU = 3;
-    private static final int GCD_GAME_MENU = 4;
-    private static final int PROGRESSION_GAME_MENU = 5;
-    private static final int PRIME_GAME_MENU = 6;
 
     public static void main(String[] args) {
         Engine.printMenu();
         Scanner sc = new Scanner(System.in);
-        int choiceNum = getNumberFromInput(sc);
+        int choiceNum = sc.nextInt();
         System.out.println("Your choice: " + choiceNum);
-        if (choiceNum == EXIT_GAME_MENU) {
-            return;
-        }
-        String userName = Cli.userMethod();
-        boolean gameResult;
-        switch (choiceNum) {
-            case EVEN_GAME_MENU:
-                gameResult = EvenGame.play();
-                break;
-            case CALC_GAME_MENU:
-                gameResult = CalcGame.play();
-                break;
-            case GCD_GAME_MENU:
-                gameResult = GcdGame.play();
-                break;
-            case PROGRESSION_GAME_MENU:
-                gameResult = ProgressionGame.play();
-                break;
-            case PRIME_GAME_MENU:
-                gameResult = PrimeGame.play();
-                break;
-            default:
-                return;
-        }
-        if (gameResult) {
-            System.out.println("Congratulations, " + userName + "!");
-        } else {
-            System.out.println("Let's try again, " + userName + "!");
-        }
+        Engine.startGame(choiceNum);
     }
 }
